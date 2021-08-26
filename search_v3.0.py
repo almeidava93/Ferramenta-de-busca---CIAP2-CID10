4a1d2c4ce5295b3b51507ae21d04d3cc4cdec37c
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2021-08-26 19:59:12 -0300

    App with search engine and feedback form

--- search_v3.0.py
+++ search_v3.0.py
@@ -8,20 +8,18 @@ import time
 import pickle
 from unidecode import unidecode
 import streamlit as st
+
+df = pd.read_parquet("text.parquet", engine="pyarrow")
+ciap_list = list(df[['CIAP2_Código1', 'titulo original']].agg(" | ".join, axis=1).drop_duplicates())
+
+
+with open('CIAP_CID_indexed_data.pkl', 'rb') as pickle_file:
+    bm25 = pickle.load(pickle_file)
+
+#Configuring database
 from google.cloud import firestore
 import uuid
 from datetime import datetime
-
-@st.cache
-def loading_data():
-    df = pd.read_parquet("text.parquet", engine="pyarrow")
-    ciap_list = list(df[['CIAP2_Código1', 'titulo original']].agg(" | ".join, axis=1).drop_duplicates())
-    with open('CIAP_CID_indexed_data.pkl', 'rb') as pickle_file:
-        bm25 = pickle.load(pickle_file)
-    return df, ciap_list, bm25
-
-df, ciap_list, bm25 = loading_data()
-
 
 #Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("firestore_key.json")
