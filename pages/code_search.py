588be32b962447f18dddd7efbadc4d85724acc3c
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2021-08-27 19:03:27 -0300

    Text update, remove search counter

--- pages/code_search.py
+++ pages/code_search.py
@@ -17,7 +17,7 @@ def app():
 
     with st.container():
         st.header('Ferramenta de busca')
-        st.write(f'Digite abaixo a condição clínica que deseja codificar e nós encontraremos para você os melhores códigos CIAP2.')
+        st.write('Digite abaixo a condição clínica que deseja codificar e nós encontraremos para você os melhores códigos CIAP2.')
         input = st.text_input('Condição clínica ou motivo de consulta:')
         n_results = st.number_input('Quantos códigos devemos mostrar?', value = 5, min_value=1, max_value=20, step=1, key=None, help='help arg')
 
