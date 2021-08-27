2bc740795fd8829b2176d2176477de08f7a1c384
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2021-08-27 16:26:08 -0300

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,8 @@ import streamlit as st
 def load_data():
     df = pd.read_parquet("text.parquet", engine="pyarrow")
     ciap_list = list(df[['CIAP2_Código1', 'titulo original']].agg(" | ".join, axis=1).drop_duplicates())
-    db = firestore.Client.from_service_account_info(dict(st.secrets['db_key']))
+    #db = firestore.Client.from_service_account_info(dict(st.secrets['db_key']))
+    db = firestore.Client.from_service_account_json('firestore_key.json')
     with open('CIAP_CID_indexed_data.pkl', 'rb') as pickle_file:
         bm25 = pickle.load(pickle_file)
     return df, ciap_list, db, search_counter, bm25
