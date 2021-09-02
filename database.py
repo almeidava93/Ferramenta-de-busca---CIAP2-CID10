8364f793312e7584a6d0c47a84052e65db893583
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2021-09-02 19:54:15 -0300

    updated database

--- database.py
+++ database.py
@@ -2,6 +2,8 @@ import pandas as pd
 from google.cloud import firestore
 import pickle
 import streamlit as st
+import pyrebase
+
 
 service_account_info = {
   "type": st.secrets['type'],
@@ -16,11 +18,20 @@ service_account_info = {
   "client_x509_cert_url": st.secrets['client_x509_cert_url']
 }
 
+firebase_storage_config = {
+  "apiKey": st.secrets['apiKey'],
+  "authDomain": st.secrets['authDomain'],
+  "projectId": st.secrets['projectId'],
+  "storageBucket": st.secrets['storageBucket'],
+  "messagingSenderId": st.secrets['messagingSenderId'],
+  "appId": st.secrets['appId'],
+  "measurementId": st.secrets['measurementId']
+}
+
 def load_data():
     df = pd.read_parquet("text.parquet", engine="pyarrow")
     ciap_list = list(df[['CIAP2_Código1', 'titulo original']].agg(" | ".join, axis=1).drop_duplicates())
     db = firestore.Client.from_service_account_info(service_account_info)
-    #db = firestore.Client.from_service_account_json('firestore_key.json')
     with open('CIAP_CID_indexed_data.pkl', 'rb') as pickle_file:
         bm25 = pickle.load(pickle_file)
     search_history = db.collection('search_history').stream()
