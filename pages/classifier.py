b3d49c776a6b6635482689e5d31bfeb9c52be7af
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2022-07-13 11:45:47 -0300

    Update classifier.py

--- pages/classifier.py
+++ pages/classifier.py
@@ -32,154 +32,6 @@ def load_data():
 
     return nlp, ciap_dict, matcher1
 
-# nlp, ciap_dict, matcher1 = load_data()
-
-
-
-
-# #Adding some customised patterns to the matcher
-# matcher2 = Matcher(matcher1.vocab)
-
-# label = '-47' # Ex.: encaminhamento para ginecologia, deseja/quer/gostaria ser encaminhado para cardio, quer fazer acompanhamento com especialista
-# pattern = [[{'POS': 'VERB', 'LEMMA': {'IN': ['gostar', 'querer', 'desejar']}}, {'OP':'?'}, {'OP':'?'}, {'OP':'?'},
-#             {'LEMMA': {'IN':['encaminhamento','encaminhar', 'acompanhamento']}}, {'POS': 'ADP', 'OP': '?'}, {'POS': 'NOUN'}]
-#     ]
-# matcher2.add(label, pattern)
-
-# label = '-60' # Ex.: resultados de um exame qualquer ou de exames
-# patterns = [
-#     [{'LEMMA': 'resultar', 'POS': 'NOUN'}, {'POS': 'ADP', 'OP': '?'}, {'POS': 'NOUN'}],
-#     [{'LEMMA': {'IN': ['avaliação', 'avaliar']}}, {'OP':'*'}, {'LEMMA': {'IN':['realizar']}}],
-#     [{'LEMMA': {'IN': ['avaliação', 'avaliar']}}, {'OP':'*'}, {'LEMMA': {'IN':['exame']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'L01' # Ex.: dor pescoço, desconforto, incômodo, tensão
-# patterns = [
-#     [{'LEMMA': {'IN':['dor', 'desconfortar', 'incomodar', 'tensão', 'rigidez']}}, {'OP':'*', 'LEMMA':{'NOT_IN':['dor', '.', 'desconfortar', 'incomodar', 'tensão', 'tensao', 'rigidez']}}, {'LEMMA': {'IN':['coluna']}, 'OP': '?'}, {'LEMMA': {'IN':['pescoço', 'cervical']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'L02' # Ex.: dor nas costas, na dorsal, no dorso
-# patterns = [
-#     [{'LEMMA': {'IN':['dor', 'desconfortar', 'incomodar', 'tensão']}}, {'OP':'*', 'LEMMA':{'NOT_IN':['dor', '.', 'desconfortar', 'incomodar', 'tensão', 'tensao']}}, {'LEMMA': {'IN':['coluna']}, 'OP': '?'}, {'LEMMA': {'IN':['costa', 'dorsal', 'dorso']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'L03' # Ex.: dor lombar, desconforto na lombar, na coluna lombar
-# patterns = [
-#     [{'LEMMA': {'IN':['dor', 'desconfortar', 'incomodar']}}, {'OP':'*', 'LEMMA':{'NOT_IN':['dor', '.', 'desconfortar', 'incomodar', 'tensão', 'tensao']}}, {'LEMMA': {'IN':['coluna']}, 'OP': '?'}, {'LEMMA': {'IN':['lombar', 'sacral', 'sacroiliaca', 'coccix', 'lombo-sacra', 'lombossacra', 'costo-lombar', 'costolombar']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'L04' # Ex.: dor torácica, desconforto
-# patterns = [
-#     [{'LEMMA': {'IN':['dor', 'desconfortar', 'incomodar', 'aperto', 'opressao']}}, {'OP':'*', 'LEMMA':{'NOT_IN':['dor', '.', 'desconfortar', 'incomodar', 'tensão', 'tensao']}}, {'LEMMA': {'IN':['peito', 'torax', 'toracico', 'toracica', 'toracicos', 'toracicas', 'esternal', 'pleural', 'pleuritica', 'costal']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'L05' # Ex.: dor axilar, desconforto na axila
-# patterns = [
-#     [{'LEMMA': {'IN':['dor', 'desconfortar', 'incomodar']}}, {'OP':'*', 'LEMMA':{'NOT_IN':['dor', '.', 'desconfortar', 'incomodar', 'tensão', 'tensao']}}, {'LEMMA': {'IN':['axila', 'axilar']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'L07' # Ex.: dor mandíbula
-# patterns = [
-#     [{'LEMMA': {'IN':['dor', 'desconfortar', 'incomodar', 'tensao', 'disfuncao', 'derrame', 'edema', 'tumefacao', 'inchaco', 'rigidez']}}, {'OP':'*', 'LEMMA':{'NOT_IN':['dor', '.', 'desconfortar', 'incomodar', 'tensão', 'tensao', 'disfuncao', 'derrame', 'edema', 'tumefacao', 'inchaco', 'rigidez']}}, {'LEMMA': {'IN':['mandíbula', 'mandibula', 'mandibular', 'ATM', 'temporo-mandibular']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'L08' # Ex.: dor ombro, desconforto ombro
-# patterns = [
-#     [{'LEMMA': {'IN':['dor', 'desconfortar', 'incomodar', 'inchaco', 'rigidez', 'derrame', 'edema', 'tumefacao']}}, {'OP':'*', 'LEMMA':{'NOT_IN':['dor', '.', 'desconfortar', 'incomodar', 'tensão', 'tensao', 'inchaco', 'rigidez']}}, {'LEMMA': {'IN':['ombro', 'acromioclavicular', 'esternoclavicular']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'L09' # Ex.: dor braço
-# patterns = [
-#     [{'LEMMA': {'IN':['dor', 'desconfortar', 'incomodar']}}, {'OP':'*', 'LEMMA':{'NOT_IN':['dor', '.', 'desconfortar', 'incomodar', 'tensão', 'tensao']}}, {'LEMMA': {'IN':['membro']}, 'OP': '?'}, {'LEMMA': {'IN':['braço', 'braco', 'bracos', 'superior']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'L10' # Ex.: dor cotovelo
-# patterns = [
-#     [{'LEMMA': {'IN':['dor', 'desconfortar', 'incomodar', 'derrame', 'edema', 'tumefacao', 'inchaco', 'rigidez']}}, {'OP':'*', 'LEMMA':{'NOT_IN':['dor', '.', 'desconfortar', 'incomodar', 'tensão', 'tensao', 'derrame', 'edema', 'tumefacao', 'inchaco', 'rigidez']}}, {'LEMMA': {'IN':['cotovelo']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'L11' # Ex.: dor punho
-# patterns = [
-#     [{'LEMMA': {'IN':['dor', 'desconfortar', 'incomodar', 'derrame', 'edema', 'tumefacao', 'inchaco', 'rigidez']}}, {'OP':'*', 'LEMMA':{'NOT_IN':['dor', '.', 'desconfortar', 'incomodar', 'tensão', 'tensao', 'derrame', 'edema', 'tumefacao', 'inchaco', 'rigidez']}}, {'LEMMA': {'IN':['punho']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'L15' # Ex.: dor joelho, desconforto no joelho
-# patterns = [
-#     [{'LEMMA': {'IN':['dor', 'desconfortar', 'incomodar', 'tensão', 'tensao', 'derrame', 'edema', 'tumefacao', 'inchaco', 'rigidez']}}, {'OP':'*', 'LEMMA':{'NOT_IN':['dor', '.', 'desconfortar', 'incomodar', 'tensão', 'tensao', 'derrame', 'edema', 'tumefacao', 'inchaco', 'rigidez']}}, {'LEMMA': {'IN':['joelho', 'joelhar', 'patela']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'L80' # Ex.: luxação/luxou patela, ombro etc
-# patterns = [
-#     [{'LEMMA': {'IN':['luxar', 'luxacao', 'luxação']}}, {'OP':'*'}, {'POS': 'NOUN'}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'N07' # Ex.: crise convulsiva, convulsão, crise de ausência
-# patterns = [
-#     [{'LEMMA': 'crise', 'POS': 'NOUN'}, {'POS': 'ADP', 'OP': '?'}, {'POS': 'NOUN', 'LEMMA': {'IN':['ausencia', 'ausência']}}],
-#     [{'LEMMA': 'crise', 'POS': 'NOUN', 'OP': '?'}, {'LEMMA': {'IN':['convulsivo', 'convulsão']}}],
-#     [{'LEMMA': {'IN': ['crise', 'convulsão', 'convulsao', 'convulsoes']}}, {'LEMMA': {'IN': ['tonico', 'tonico-clonica', 'tonico-clonico', 'tonico-clonicos', 'tonico-clonicas']}}, {'LEMMA': {'IN': ['clonica', 'clonico']}, 'OP': '?'}, {'LEMMA': {'IN': ['generalizar']}, 'OP': '?'}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'P74' # Ex.: crise de ansiedade, crise ansiosa
-# patterns = [
-#     [{'LEMMA': {'IN': ['crise']}, 'POS': {'IN': ['NOUN']}}, {'OP':'*', 'LEMMA':{'NOT_IN':['crise']}, 'IS_PUNCT': False}, {'LEMMA':{'IN': ['ansiedade', 'ansioso']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'P77' # Ex.: tentativa de suicídio
-# patterns = [
-#     [{'LEMMA': {'IN': ['tentativo', 'tentar', 'pensar', 'pensamento', 'desejar', 'querer']}, 'POS': {'IN': ['NOUN', 'VERB']}}, {'OP':'*'}, {'LEMMA': 'se', 'OP': '?'}, {'LEMMA':{'IN': ['suicidio', 'suicidios', 'suicidar-se', 'suicidar', 'matar', 'morrer', 'morte']}}],
-#     [{'LEMMA': {'IN': ['ideacao', 'ideacoes', 'ideação']}, 'POS': 'NOUN'}, {'LEMMA': 'suicidar', 'POS': 'ADJ'}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'S08' # Ex.: manchas na pele, nos pés, no rosto
-# patterns = [
-#     [{'LEMMA': 'manchar', 'POS': 'NOUN'}, {'OP':'*'}, {'POS': 'NOUN'}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'S21' # Ex.: queda de cabelo
-# patterns = [
-#     [{'LEMMA': 'pelar', 'POS': 'NOUN'}, {'OP':'*'}, {'LEMMA': {'IN': ['descamar']},'POS': 'VERB'}],
-#     [{'LEMMA': {'IN': ['descamação', 'descamacao']}}, {'OP':'*'}, {'LEMMA': 'pelar', 'POS': 'NOUN'}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'S23' # Ex.: queda de cabelo
-# patterns = [
-#     [{'LEMMA': 'cabelo', 'POS': 'NOUN'}, {'OP':'*'}, {'LEMMA': {'IN': ['cair']},'POS': 'VERB'}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'X04' # Ex.: dor na relação sexual, dor no sexo
-# patterns = [
-#     [{'LEMMA': 'dor', 'POS': 'NOUN'}, {'OP':'*'}, {'LEMMA': {'IN': ['sexo', 'sexual']},'POS':{'IN': ['ADJ', 'NOUN']}}]
-# ]
-# matcher2.add(label, patterns)
-
-# label = 'X18' # Ex.: dor nas mamas
-# patterns = [
-#     [{'LEMMA': 'dor', 'POS': 'NOUN'}, {'OP':'*'}, {'LEMMA': {'IN': ['mamar', 'mamário']},'POS':{'IN': ['ADJ', 'VERB', 'NOUN']}}]
-# ]
-# matcher2.add(label, patterns)
-
-
-
 
 @st.cache(ttl=None, show_spinner=True)
 def join_columns(dataframe, column_names, delimiter=' | ', drop_duplicates=False):
@@ -286,6 +138,23 @@ def prepare_matcher():
         [{'LEMMA': {'IN': ['crise', 'convulsão', 'convulsao', 'convulsoes']}}, {'LEMMA': {'IN': ['tonico', 'tonico-clonica', 'tonico-clonico', 'tonico-clonicos', 'tonico-clonicas']}}, {'LEMMA': {'IN': ['clonica', 'clonico']}, 'OP': '?'}, {'LEMMA': {'IN': ['generalizar']}, 'OP': '?'}]
     ]
     matcher2.add(label, patterns)
+
+    label = 'P01' # Ex.: sentir-se agitado, nervoso
+    patterns = [
+        [{'LEMMA': {'IN': ['sentir']}}, {'LEMMA': {'IN': ['agitar', 'nervoso']}}]
+    ]
+    
+    label = 'P15' # Ex.: etilismo, consumo de álcool
+    patterns = [
+        [{'LEMMA':{'IN': ['consumir']}}, {'OP':'*', 'IS_PUNCT': False}, {'LEMMA':{'IN': ['álcool', 'alcool']}}],
+        [{'LEMMA':{'IN': ['consumir']}}, {'OP':'*', 'IS_PUNCT': False}, {'LEMMA':{'IN': ['bebido']}}, {'LEMMA':{'IN': ['alcoolica','alcoolicas']}}]
+    ]
+
+    label = 'P17' # Ex.: tabagismo, consumo de cigarro
+    patterns = [
+        [{'LEMMA':{'IN': ['consumir']}}, {'OP':'*', 'IS_PUNCT': False}, {'LEMMA':{'IN': ['cigarrar']}}]
+    ]
+
 
     label = 'P74' # Ex.: crise de ansiedade, crise ansiosa
     patterns = [
